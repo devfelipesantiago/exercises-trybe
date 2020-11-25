42438bbc0cd733d09d44d162f0e4d4215aeed5ff
devfelipesantiago <felipesantiagolds@gmail.com>
2020-11-25 18:53:08 -0300

    Exercicio 1

---
+++ Fundamentos_de_desenvolvimento_web/bloco_4/dia_1/faca_5_programas.js
@@ -0,0 +1,18 @@
+//Variaveis
+let a = 10;
+let b = 20;
+//soma
+let sum = a + b;
+console.log(sum);
+//subtração
+let sub = a - b;
+console.log(sub);
+//multiplicação
+let mul = a * b;
+console.log(mul);
+//divisão
+let div = a / b;
+console.log(div);
+//mod
+let mod = a % b;
+console.log(mod);
