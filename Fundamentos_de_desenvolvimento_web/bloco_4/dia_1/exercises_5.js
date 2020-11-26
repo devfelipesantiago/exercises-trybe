bab805a3da09a3cd57b0bb4e75935c207e080d38
devfelipesantiago <felipesantiagolds@gmail.com>
2020-11-25 21:34:45 -0300

    Exercicio 5

---
+++ Fundamentos_de_desenvolvimento_web/bloco_4/dia_1/exercises_5.js
@@ -0,0 +1,11 @@
+//Variavel
+let a = 10;
+let b = 40;
+let c = 100;
+triangulo = a + b + c;
+//True or false
+if (triangulo == 180) {
+  console.log("É " + true + " este triangulo")
+} else {
+  console.log("É " + false + " este triangulo");
+}
