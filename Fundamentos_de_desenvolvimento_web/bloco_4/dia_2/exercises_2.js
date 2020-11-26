e4ea0276cbf51502e30bd00b6b456970c9153f70
devfelipesantiago <felipesantiagolds@gmail.com>
2020-11-26 16:33:13 -0300

    Exercicio 2

---
+++ Fundamentos_de_desenvolvimento_web/bloco_4/dia_2/exercises_2.js
@@ -0,0 +1,10 @@
+/*Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;*/
+//Array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let amount = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  let sum = numbers[i]; 
+  amount += sum;
+}
+console.log(amount);
