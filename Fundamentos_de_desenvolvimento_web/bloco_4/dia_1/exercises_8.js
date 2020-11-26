92097f731724e7a0e328ccc5037af96a23fe9521
devfelipesantiago <felipesantiagolds@gmail.com>
2020-11-25 23:34:49 -0300

    Exercicio 8 corrigido

--- Fundamentos_de_desenvolvimento_web/bloco_4/dia_1/exercises_8.js
+++ Fundamentos_de_desenvolvimento_web/bloco_4/dia_1/exercises_8.js
@@ -3,11 +3,11 @@ let number1 = 1;
 let number2 = 21;
 let number3 = 3;
 
+let even = false;
 //even or odd
 if (number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0) {
-  console.log("even");
-} else {
-  console.log("odd");
+  even = true;
 }
+console.log(even);
 
 /*Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .*/
