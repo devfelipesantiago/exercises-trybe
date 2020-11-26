5bdf85957c1fd5e6369cfff486b675b59d2475be
devfelipesantiago <felipesantiagolds@gmail.com>
2020-11-25 21:23:37 -0300

    Exercicio 4

---
+++ Fundamentos_de_desenvolvimento_web/bloco_4/dia_1/exercises_4.js
@@ -0,0 +1,10 @@
+//Variavel
+let a = -10;
+//É positivo, negativo ou zero?
+if (a == 0) {
+  console.log("Zero")
+} else if (a < 0) {
+  console.log("Negativo")
+} else {
+  console.log("Positivo")
+}
