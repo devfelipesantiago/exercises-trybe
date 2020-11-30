2897837d63feb9c662a479f725a91bdbcee7bef6
devfelipesantiago <felipesantiagolds@gmail.com>
2020-11-30 20:04:45 -0300

    Exercicio for in

--- Fundamentos_de_desenvolvimento_web/bloco_4/introducao-a-javascript-e-logica-de-programacao.4/exercises_for_in.js
+++ Fundamentos_de_desenvolvimento_web/bloco_4/introducao-a-javascript-e-logica-de-programacao.4/exercises_for_in.js
@@ -2,12 +2,13 @@ let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: 'Sim'
 };
-
+/*
 console.log("Bem-vinda " + info.personagem);
 console.log("1-----------------------------------------------------");
 
-info.recorrente = 'Sim';
+
 
 console.log(info);
 console.log('2-----------------------------------------------------');
@@ -20,15 +21,23 @@ console.log('3-----------------------------------------------------');
 for (let key in info) {
   console.log(info[key]);
 }
-console.log('4-----------------------------------------------------');
+console.log('4-----------------------------------------------------');*/
 
 let info2 = {
-  personagem: "Pato Donald",
-  origem: "Christimas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: "O ultimo MacPatinhas",
-  recorrente: 'Sim'
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim"
+};
+
+for (let properties in info) {
+  if (
+    info[properties] === info.recorrente &&
+    info[properties] === "Sim" &&
+    info2[properties] === "Sim"
+  ) {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[properties] + " e " + info2[properties]);
+  }
 }
-console.log(info.personagem + ' e ' + info2.personagem);
-console.log(info2.personagem + ' e ' + info2.origem);
-console.log(info.nota + ' e ' + info2.nota);
-console.log('Ambos recorrentes ' + info.recorrente);
