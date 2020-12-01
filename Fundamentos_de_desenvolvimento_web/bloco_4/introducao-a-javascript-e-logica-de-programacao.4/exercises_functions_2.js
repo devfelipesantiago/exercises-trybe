accdcbefaa71c4c3ff544b4c9e91038019f534bf
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-01 08:28:45 -0300

    Exercicio funçoes 2

--- Fundamentos_de_desenvolvimento_web/bloco_4/introducao-a-javascript-e-logica-de-programacao.4/exercises_functions_2.js
+++ Fundamentos_de_desenvolvimento_web/bloco_4/introducao-a-javascript-e-logica-de-programacao.4/exercises_functions_2.js
@@ -1,11 +1,11 @@
-function lowerValue(array) {
+function highestValue(array) {
   let value = 0;
   for (let i = 0; i < array.length; i++) {
     if (value < array[i]) {
-      value = array[i]
+      value = array[i];
     }
   }
   return value;
 }
 
-console.log(lowerValue([2, 30, 6, 79, 10, 1]));
+console.log(highestValue([2, 30, 6, 79, 10, 1]));
