a32a4b4b674f2e5bd32132288305af997383ad29
santiago <felipesantiagolds@gmail.com>
2021-02-05 14:04:50 -0300

    Organizando diretórios

--- Fundamentos_de_desenvolvimento_web/bloco_10_Testes_automatizados_com_Jest/Primeiros_passos_no_Jest/part001.spec.js
+++ Fundamentos_de_desenvolvimento_web/bloco_10_Testes_automatizados_com_Jest/Primeiros_passos_no_Jest/part001.spec.js
@@ -1,4 +1,4 @@
-const sum = require('./parte001');
+const sum = require('./part001');
 /*
 A função sum(a, b) retorna a soma do parâmetro a com o b
 Teste se o retorno de sum(4, 5) é 9
@@ -9,11 +9,11 @@ Teste se a mensagem de erro é "parameters must be numbers" quando realizar a ch
 
 // implemente seus testes aqui
 describe('Parte 1', () => {
-    it('retorna a soma do parâmetro a com o', () => {
+    it('retorna a soma do parâmetro a com o b', () => {
         expect(2).toEqual(sum(1, 1));
     });
     test('o retorno de sum(4, 5) é 9', () => {
-        expect(sum(4, 5)).toEqual(9);
+        expect(sum(4, 5)).toBe(9);
     });
     it('o retorno de sum(0, 0) é 0', () => {
         expect(sum(0, 0));
@@ -23,5 +23,5 @@ describe('Parte 1', () => {
     });
     it('A mensagem de erro é "parameters must be numbers', () => {
         expect(() => { sum(4, '5'); }).toThrow(/parameters must be numbers/);
-    })
+    });
 });
