e1bfbc988afa559ebfdb190d91f01d7a95226df2
santiago <felipesantiagolds@gmail.com>
2021-02-22 16:30:51 -0300

    this, constructor and bind

--- Desenvolvimento_Front-end/bloco_12/fixed/src/App.js
+++ Desenvolvimento_Front-end/bloco_12/fixed/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
+    Com isso, podemos acessar as `props`, estado do componente (ainda vamos ver como!)
+    e tudo o mais daqui de dentro */
+    console.log(this);
+    console.log('Clicou!');
+  }
+
+  render() {
+    return <button onClick={ this.handleClick }>Meu botão</button>;
+  }
 }
 
 export default App;
