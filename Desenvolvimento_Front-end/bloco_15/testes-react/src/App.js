656e164ff2273dab7bbdc83f59f1fec72710a448
santiago <felipesantiagolds@gmail.com>
2021-03-20 22:11:01 -0300

    Exemplos do course

--- Desenvolvimento_Front-end/bloco_15/testes-react/src/App.js
+++ Desenvolvimento_Front-end/bloco_15/testes-react/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import ValidEmail from './ValidEmail'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' });
+  }
+
+  renderText(saveEmail) {
+    if (saveEmail) return <ValidEmail email={ saveEmail } />;
+
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App" >
+        <label htmlFor="id-email">
+          Email
+          </label>
+        <input id="id-email" type="email" value={ email } onChange={ (e) => this.changeEmail(e.target.value) } />
+        <input id="btn-send" type="button" value="Enviar"
+          data-testid="id-send" onClick={ () => this.changeSaveEmail(email) } />
+        <input id="btn-back" type="button" value="Voltar" />
+        { this.renderText(saveEmail) }
+        {/* <ValidEmail email={ saveEmail } /> */ }
+      </div>
+    );
+  }
 }
 
 export default App;
