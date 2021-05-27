2d77bc3b8acfabedc8935b343541583b89d141c5
santiago <felipesantiagolds@gmail.com>
2021-05-27 10:15:16 -0300

    Exercises 21.3

--- Desenvolvimento_Back-End/21.3/exercises.sql
+++ Desenvolvimento_Back-End/21.3/exercises.sql
@@ -29,3 +29,16 @@ SET NEW.ticket_price_estimation = IF (NEW.ticket_price > OLD.ticket_price, 'Incr
   VALUES(NEW.movie_id, 'UPDATE', NOW());
   END $$
 DELIMITER ;
+--
+DELIMITER $$
+
+CREATE TRIGGER trigger_movie_delete
+    BEFORE DELETE ON movies
+    FOR EACH ROW
+BEGIN
+    INSERT INTO movies_logs(movie_id, executed_action, log_date)
+    VALUES(OLD.movie_id, 'DELETE', NOW());
+END $$
+
+DELIMITER ;
+--
