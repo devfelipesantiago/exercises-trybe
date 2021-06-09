96713703de77aeeb63af3457cd71d32b21268249
santiago <felipesantiagolds@gmail.com>
2021-06-09 16:32:51 -0300

    exercises 6

---
+++ Desenvolvimento_Back-End/24.3/exercises6.js
@@ -0,0 +1,2 @@
+// Retorne todos os filmes com ratings maior do que 103 , exibindo apenas os campos title e ratings .
+db.movies.find({ ratings: { $elemMatch: { $gt: 103 } } }, { title: true, ratings: true });
