c0325e7770e2844b44827c57d97da89b5ec2368d
Felipe <felipesantiagolds@gmail.com>
2021-06-24 13:43:41 -0300

    Exercises 4

--- Desenvolvimento_Back-End/HTTP_nodejs/exercises-express/index.js
+++ Desenvolvimento_Back-End/HTTP_nodejs/exercises-express/index.js
@@ -10,3 +10,10 @@ app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({ 'message': `Hello, ${name}!`})
 })
+
+app.post('/greetings', (res, req) => {
+  const { name, age } = req.body;
+
+  if (parseInt(age, 10) >= 17) res.json({ 'message': `Hello, ${name}!` });
+  res.status(401).json({ 'message': `Unauthorized` })
+})
