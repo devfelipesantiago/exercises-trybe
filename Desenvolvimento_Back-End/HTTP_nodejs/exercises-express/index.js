bdd7700a28ac290bbedefd8e883317909bc35c1d
Felipe <felipesantiagolds@gmail.com>
2021-06-30 16:13:55 -0300

    'Exercicios express'

--- Desenvolvimento_Back-End/HTTP_nodejs/exercises-express/index.js
+++ Desenvolvimento_Back-End/HTTP_nodejs/exercises-express/index.js
@@ -3,25 +3,25 @@ const express = require('express');
 const app = express();
 
 app.get('/ping', (req, res) => {
-  res.json({'message': 'pong'});
+  res.json({ 'message': 'pong' });
 });
 
-// app.post('/hello', (req, res) => {
-//   const { name } = req.body;
-//   res.status(200).json({'message': `Hello, ${name}!`});
-// });
+app.post("/hello", (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}!` });
+});
 
-// app.post('/greetings', (res, req) => {
-//   const {name,  age} = req.body;
+app.post('/greetings', (res, req) => {
+  const { name, age } = req.body;
 
-//   if (parseInt(age, 10) >= 17) res.json({'message': `Hello, ${name}!`});
-//   res.status(401).json({'message': `Unauthorized`});
-// });
+  if (parseInt(age, 10) >= 17) res.json({ 'message': `Hello, ${name}!` });
+  res.status(401).json({ 'message': `Unauthorized` });
+});
 
-// app.put('/users/:name/:age', (req, res) => {
-//   const {name, age} = req.body;
-//   res.status(200).json({'message': `Seu nome é ${name} e você tem ${age} anos de idade`});
-// });
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.body;
+  res.status(200).json({ 'message': `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
 
 app.listen(3000, () => {
   console.log('Ouvindo a porta 3000');
