5d64909289c50fe37c57c7a2090dc2b43f249ef3
Felipe <felipesantiagolds@gmail.com>
2021-07-06 15:25:06 -0300

    Refactor e Test Doubles

--- Desenvolvimento_Back-End/Arquitetura_software/test-camadas/tests/model/movieModel.test.js
+++ Desenvolvimento_Back-End/Arquitetura_software/test-camadas/tests/model/movieModel.test.js
@@ -1,33 +1,25 @@
 const { expect } = require('chai');
-
-/*
-  Como ainda não temos a implementação, vamos fixar
-  um objeto simulando os métodos que iremos desenvolver,
-  porém, eles não terão nenhum comportamento
-*/
-const MoviesModel = {
-  create: () => {}
-};
+const MoviesModel = require('../../model/movieModel');
 
 describe('Insere um novo filme no BD', () => {
   const payloadMovie = {
     title: 'Example Movie',
     directedBy: 'Jane Dow',
     releaseYear: 1999,
-  }
+  };
 
   describe('quando é inserido com sucesso', async () => {
 
     it('retorna um objeto', async () => {
       const response = await MoviesModel.create(payloadMovie);
 
-      expect(response).to.be.a('object')
+      expect(response).to.be.a('object');
     });
 
     it('tal objeto possui o "id" do novo filme inserido', async () => {
       const response = await MoviesModel.create(payloadMovie);
 
-      expect(response).to.have.a.property('id')
+      expect(response).to.have.a.property('id');
     });
 
   });
