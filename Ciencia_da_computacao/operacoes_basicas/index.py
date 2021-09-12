33f817c1760e01ffd00bd251c824d778d728f835
Felipe <felipe@pop-os.localdomain>
2021-09-12 14:46:22 -0300

    Operacoes básicas em Python

---
+++ Ciencia_da_computacao/operacoes_basicas/index.py
@@ -0,0 +1,20 @@
+square_root = 25 ** (1 / 2)
+print(square_root)
+counter = 0
+counter += 1
+print(counter)
+
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+print(hours)
+print(minutes)
+print(seconds)
