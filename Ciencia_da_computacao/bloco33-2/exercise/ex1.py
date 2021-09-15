49d3a9052a9eb639567d8b7e888f94e07b95d73f
Felipe <felipe@pop-os.localdomain>
2021-09-15 18:18:28 -0300

    Json e CSV

---
+++ Ciencia_da_computacao/bloco33-2/exercise/ex1.py
@@ -0,0 +1,7 @@
+def vertical_name(string):
+    charList = string.split("")
+    for letter in charList:
+        print(letter)
+
+
+vertical_name("PEDRO")
---
+++ Ciencia_da_computacao/bloco33-2/json_csv/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+with open("balneabilidade.csv") as file:
+#     beach_status_reader = csv.reader(file, delimiter=",", quotechar='"')
+#     header, *data = [beach_status_reader]
+
+# print(data)
