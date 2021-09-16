0af6c223d9ea0151886aeb8167ea93e882d3c6b8
Felipe <felipe@pop-os.localdomain>
2021-09-15 21:27:09 -0300

    Json e CSV

--- Ciencia_da_computacao/bloco33-2/exercise/ex1.py
+++ Ciencia_da_computacao/bloco33-2/exercise/ex1.py
@@ -1,7 +1,21 @@
+from abc import ABC, abstractmethod
+
+
 def vertical_name(string):
-    charList = string.split("")
-    for letter in charList:
+    for letter in string:
         print(letter)
 
 
 vertical_name("PEDRO")
+
+
+class test:
+    def __init__(self, name):
+        self.__name
+
+    def do_test(self):
+        return self.__name
+
+
+class test2(test):
+    test.do_test
--- Ciencia_da_computacao/bloco33-2/json_csv/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-with open("balneabilidade.csv") as file:
-#     beach_status_reader = csv.reader(file, delimiter=",", quotechar='"')
-#     header, *data = [beach_status_reader]
-
-# print(data)
