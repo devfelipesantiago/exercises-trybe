7f75a6c51701a5777c752fc13c125bc80dd726bc
Felipe <felipe@pop-os.localdomain>
2021-09-16 16:59:36 -0300

    ex1

--- Ciencia_da_computacao/bloco33-2/exercise/ex1.py
+++ Ciencia_da_computacao/bloco33-2/exercise/ex1.py
@@ -1,21 +1,4 @@
-from abc import ABC, abstractmethod
+NAME = "PEDRO"
 
-
-def vertical_name(string):
-    for letter in string:
-        print(letter)
-
-
-vertical_name("PEDRO")
-
-
-class test:
-    def __init__(self, name):
-        self.__name
-
-    def do_test(self):
-        return self.__name
-
-
-class test2(test):
-    test.do_test
+for letter in NAME:
+    print(letter)
