5dc401e8e5df0939bb6fb8fa873abd6c870516f8
Felipe Santiago <devfelipesantiago@gmail.com>
2022-06-18 21:44:52 -0300

    delete

--- Desenvolvimento_Front-end/bloco_11/introdução-react/first-component/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Component from './component';
-
-function App() {
-  return <Component />;
-}
-
-export default App;
--- Desenvolvimento_Front-end/bloco_11/introdução-react/hello-world-react/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (<span className="HelloWorld">Hello, world!</span>);
-}
-
-export default App;
