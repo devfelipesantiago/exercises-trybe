0642402fd12e2763d397fca638e5bff591e0643e
felipe santiago <devfelipesantiago@gmail.com>
2022-09-10 15:45:41 -0300

    bloco 14 primeiros passos RTL

---
+++ Desenvolvimento_Front-end/bloco_14/todo-list/src/test/exerciseOne.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+const TASK_DESCRIPTION = 'Beber água!';
+
+describe('Testando a aplicação, testando o botão e sua funcionalidade', () => {
+  test('Verifica se o botão está na tela com o texto "Adicionar"', () => {
+    render(<App />);
+    const buttonAdd = screen.getByText('Adicionar');
+    expect(buttonAdd).toBeInTheDocument();
+    expect(buttonAdd.type).toBe('button');
+  });
+
+  test('Ao clicar no botão "Adicionar" a task deve ser adicionada na tela', () => {
+    render(<App />);
+
+    const inputTask = screen.getByLabelText('Tarefa:');
+    const buttonAdd = screen.queryByText('Adicionar');
+    userEvent.type(inputTask, TASK_DESCRIPTION);
+    expect(screen.queryByText(TASK_DESCRIPTION)).not.toBeInTheDocument();
+    userEvent.click(buttonAdd);
+    expect(screen.queryByText(TASK_DESCRIPTION)).toBeInTheDocument();
+  });
+});
