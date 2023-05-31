486bb656b4a7627eb5164de321eb242506c8a5e1
Felipe Santiago <devfelipesantiago@gmail.com>
2023-05-31 19:45:02 -0300

    Funçao exec e readline package

--- Desenvolvimento_Back-End/typescript/my-ts-scripts/capacity.ts
+++ Desenvolvimento_Back-End/typescript/my-ts-scripts/capacity.ts
@@ -7,6 +7,7 @@ valor - number
 unidade base - string
 unidade para a conversão - string
  */
+import readline from 'readline-sync';
 const unidades = [ 'kl', 'hl', 'dal', 'l', 'dl', 'cl', 'ml' ];
 
 export function convert(value: number, fromUnit: string, toUnit: string): number {
@@ -16,3 +17,33 @@ export function convert(value: number, fromUnit: string, toUnit: string): number
 
   return value * Math.pow(10, exponent);
 }
+
+function exec() {
+  // pegamos o valor a ser convertido digitado pela pessoa usuária
+  const value = readline.questionFloat('Digite o valor a ser convertido: \n');
+
+  // pedimos que a pessoa usuária escolha a unidade base
+  const fromUnitChoiceIndex = readline.keyInSelect(unidades, 'Escolha um número para a unidade base:');
+
+  // pedimos que a pessoa usuária escolha a unidade para conversão
+  const toUnitChoiceIndex = readline.keyInSelect(unidades, 'Escolha um número para a conversão:');
+
+  const toUnitChoice = unidades[ toUnitChoiceIndex ];
+  const fromUnitChoice = unidades[ fromUnitChoiceIndex ];
+
+  // Se o usuário escolher a opção 0 (cancelar), uma mensagem é impressa no terminal e usamos o return para encerrar a execução
+  if (!fromUnitChoice || !toUnitChoice) {
+    console.log(`Função cancelada`);
+    return 0;
+  }
+
+  const result = convert(value, fromUnitChoice, toUnitChoice);
+
+  // montamos a mensagem de saída
+  const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}`;
+
+  // printamos a mensagem de saída no terminal
+  console.log(message);
+}
+
+exec();
--- Desenvolvimento_Back-End/typescript/my-ts-scripts/volume.ts
+++ Desenvolvimento_Back-End/typescript/my-ts-scripts/volume.ts
@@ -1,5 +1,5 @@
 // ./volume.ts
-
+import readline from 'readline-sync';
 const unidades = [ 'km³', 'hm³', 'dam³', 'm³', 'dm³', 'cm³', 'mm³' ];
 
 function convert(value: number, fromUnit: string, toUnit: string): number {
@@ -9,3 +9,33 @@ function convert(value: number, fromUnit: string, toUnit: string): number {
 
   return value * Math.pow(1000, exponent);
 }
+
+function exec() {
+  // pegamos o valor a ser convertido digitado pela pessoa usuária
+  const value = readline.questionFloat('Digite o valor a ser convertido: \n');
+
+  // pedimos que a pessoa usuária escolha a unidade base
+  const fromUnitChoiceIndex = readline.keyInSelect(unidades, 'Escolha um número para a unidade base:');
+
+  // pedimos que a pessoa usuária escolha a unidade para conversão
+  const toUnitChoiceIndex = readline.keyInSelect(unidades, 'Escolha um número para a conversão:');
+
+  const toUnitChoice = unidades[ toUnitChoiceIndex ];
+  const fromUnitChoice = unidades[ fromUnitChoiceIndex ];
+
+  // Se o usuário escolher a opção 0 (cancelar), uma mensagem é impressa no terminal e usamos o return para encerrar a execução
+  if (!fromUnitChoice || !toUnitChoice) {
+    console.log(`Função cancelada`);
+    return 0;
+  }
+
+  const result = convert(value, fromUnitChoice, toUnitChoice);
+
+  // montamos a mensagem de saída
+  const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}`;
+
+  // printamos a mensagem de saída no terminal
+  console.log(message);
+}
+
+exec();
